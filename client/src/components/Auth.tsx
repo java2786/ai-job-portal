104e474938492e15935abe4b937edab40359b2fc
Arun Kumar <java.kumar.arun@gmail.com>
2026-09-14 19:25:20 +0530

    Login Page Inupt type pwd

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -53,8 +53,8 @@ function Auth({onLoginSuccess}: AuthProps){
 
             <form onSubmit={handleSubmit}>
                 {isSignup && <input placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}/>}
-                <input placeholder="Email address" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                <input placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <input type="email" placeholder="Email address" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
 
                 {isSignup && <select onChange={(e)=>setRole(e.target.value as "admin"|"candidate")}>
                     <option value='candidate'>Candidate</option>
